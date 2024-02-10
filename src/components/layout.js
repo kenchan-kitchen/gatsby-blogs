ec817e5f52d0da113aede4fd623f8a3709c0c040
kenchan <ken.letsroll.econ.pc@gmail.com>
2024-02-10 15:48:50 +0900

    create footer.js and modify layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,12 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Header from "./header"
+import Footer from "./footer"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  
+
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
